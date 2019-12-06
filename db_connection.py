92ae5871ae4fc14f7fec9a94774a139f462227d0
Spence Guo <37296692+SpenceGuo@users.noreply.github.com>
2019-12-06 22:30:49 +0800

    Initial commit

---
+++ db_connection.py
@@ -0,0 +1,46 @@
+import pymysql
+import pymongo
+import json
+
+servername = "10.10.11.2"
+port = "23306"
+username = "qi"
+password = "password"
+dbname = "SH_Stock_Exchange"
+jsonfile_savepath = 'D:/GithubProjects/financial/json/IC0001.json'
+stock_id = 'IC0001.json'
+
+stock_json = []
+
+str_c = '\''
+sql = 'SELECT datetime,preclosepx,openpx,highpx,lowpx,lastpx FROM his_cff WHERE securityid=\'IC0001\' ORDER BY datetime DESC'
+
+db = pymysql.connect(servername, username, password, dbname)
+cursor = db.cursor()
+cursor.execute(sql)
+data = cursor.fetchall()
+for row in data:
+    onedata = {}
+
+    datetime = row[0]
+    preclosepx = row[1]
+    openpx = row[2]
+    highpx = row[3]
+    lowpx = row[4]
+    lastpx = row[5]
+
+    onedata['datetime'] = datetime
+    onedata['preclosepx'] = preclosepx
+    onedata['openpx'] = openpx
+    onedata['highpx'] = highpx
+    onedata['lowpx'] = lowpx
+    onedata['lastpx'] = lastpx
+    stock_json.append(onedata)
+
+    print("JSON 对象：", json.dumps(stock_json))
+    # print(datetime, preclosepx, openpx, highpx, lowpx, lastpx, end='\n')
+db.close()
+
+f = open(jsonfile_savepath, 'w')
+f.write(json.dumps(stock_json))
+f.close()
