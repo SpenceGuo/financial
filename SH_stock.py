3ba3ea4e90e3286e3d9e0b6760fe1f96110c809a
Spence Guo <37296692+SpenceGuo@users.noreply.github.com>
2019-12-10 16:19:43 +0800

    update

--- SH_stock.py
+++ SH_stock.py
@@ -1,39 +1,41 @@
 import db_config
 import pymysql
 
-host = db_config.host
-port = db_config.port
-username = db_config.username
-password = db_config.password
-dbname = db_config.dbname
+class Stock:
+    host = db_config.host
+    port = db_config.port
+    username = db_config.username
+    password = db_config.password
+    dbname = db_config.dbname
 
-filepath = ''
+    def getData(self, securityid):
+        data = []
+        sql = 'SELECT datetime,openpx,lastpx,lowpx,highpx FROM his_cff WHERE securityid=\'' + securityid + '\'' + ' ORDER BY datetime ASC'
 
-data = []
+        db = pymysql.connect(self.host, self.username, self.password, self.dbname)
+        cursor = db.cursor()
+        cursor.execute(sql)
 
-sql = 'SELECT datetime,openpx,lastpx,lowpx,highpx FROM his_cff WHERE securityid=\'IC0001\' ORDER BY datetime DESC'
+        result = cursor.fetchall()
 
-db = pymysql.connect(host, username, password, dbname)
-cursor = db.cursor()
-cursor.execute(sql)
+        for row in result:
+            onedata = []
 
-result = cursor.fetchall()
+            datetime = row[0]
+            openpx = row[1]
+            lastpx = row[2]
+            lowpx = row[3]
+            highpx = row[4]
 
-for row in result:
-    onedata = []
+            onedata.append(datetime)
+            onedata.append(openpx)
+            onedata.append(lastpx)
+            onedata.append(lowpx)
+            onedata.append(highpx)
 
-    datetime = row[0]
-    openpx = row[1]
-    lastpx = row[2]
-    lowpx = row[3]
-    highpx = row[4]
+            data.append(onedata)
 
-    onedata.append(datetime)
-    onedata.append(openpx)
-    onedata.append(lastpx)
-    onedata.append(lowpx)
-    onedata.append(highpx)
+        return data
 
-    data.append(onedata)
 
-print(data)
+
